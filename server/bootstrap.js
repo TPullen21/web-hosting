23d5e3dae7257162d5579a5fe804bb069551db42
Tom Pullen <tpullen21@gmail.com>
2016-01-20 20:57:34 +0000

    Added default admin user functionality on startup and set up routes

---
+++ server/bootstrap.js
@@ -0,0 +1,26 @@
+Meteor.startup(function() {
+	if (Meteor.users.find().count() < 1) {
+		var users = [
+			{
+				name: "Superuser",
+				email: "admin@example.com",
+				roles: ['admin']
+			}
+		];
+
+		_.each(users, function(user) {
+			var id = Accounts.createUser({
+				email: user.email,
+				password: "password",
+				profile: {
+					name: user.name
+				}
+			});
+
+			if (user.roles.length > 0) {
+				Roles.addUsersToRoles(id, user.roles);
+			}
+
+		});
+	}
+});
